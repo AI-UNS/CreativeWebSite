664c245b2a5db9ddbc776c2c09afebc33e2f3ecf
UNS <u.nurakbar97@gmail.com>
2021-02-07 19:58:04 +0500

    media adjustment in header, about

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
-let bars = document.querySelectorAll('.statis__content-bar');
+let bars = document.querySelectorAll('.statis__content-bar'),
+    burgerMenu = document.querySelector('.burger_menu');
+    burgerMenuList = document.querySelector('.burger_menu-list'),
+    spanDate = document.querySelectorAll('.date');
 
 for(let i = 0; i < bars.length; i++){
     bars[i].style = `width: ${90 + i * 5}%;`;
-    // console.log(bars[i].style = `width: ${100 - i * 5}%;`)
 }
+
+burgerMenu.addEventListener('click', function () {
+    burgerMenuList.classList.toggle('visibility');
+})
+
+
+let time = new Date(), day = time.getDate(), month = time.getMonth(), year = time.getFullYear();
+monthes = {0 : 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Apr', 4: 'May', 5: 'Jun', 6: 'Jul', 7: 'Aug', 8: 'Sep', 9: 'Oct', 10: 'Nov', 11: 'Dec'};
+
+let literalMonth;
+
+for(let key in monthes){
+    if(month == key){
+        literalMonth = monthes[key];
+     }
+}
+
+for(let i = 0; i < spanDate.length; i++){
+    currentDate = day.toString() + ' ' + literalMonth + ' ' + year.toString();
+    spanDate[i].innerHTML = currentDate;
+}
+
+
+console.log(spanDate);
